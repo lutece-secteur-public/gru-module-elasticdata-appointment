2c4ffb2608ab698209af433db2e5df480fa47443
laurent payen <laurent.payen@gmail.com>
2017-07-20 11:45:43 +0200

    Migration compatibility with plugin appointment 2.0.0-SNAPSHOT

--- src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
+++ src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
@@ -3,9 +3,10 @@ package fr.paris.lutece.plugins.elasticdata.modules.appointment.business;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import fr.paris.lutece.plugins.appointment.business.Appointment;
+
 import fr.paris.lutece.plugins.appointment.business.AppointmentFilter;
-import fr.paris.lutece.plugins.appointment.business.AppointmentHome;
+import fr.paris.lutece.plugins.appointment.business.appointment.Appointment;
+import fr.paris.lutece.plugins.appointment.service.AppointmentService;
 import fr.paris.lutece.plugins.elasticdata.business.AbstractDataSource;
 import fr.paris.lutece.plugins.elasticdata.business.DataObject;
 import fr.paris.lutece.plugins.elasticdata.business.DataSource;
@@ -20,7 +21,7 @@ public class AppointmentDataSource extends AbstractDataSource implements DataSou
         AppointmentFilter appointmentFilter = new AppointmentFilter( );
         Collection<DataObject> collResult = new ArrayList<DataObject>( );
 
-        for ( Appointment appointment : AppointmentHome.getAppointmentListByFilter( appointmentFilter ) )
+        for ( Appointment appointment : AppointmentService.findListAppointmentsByFilter(appointmentFilter ) )
         {
 
             collResult.add( new AppointmentDataObject( appointment ) );
