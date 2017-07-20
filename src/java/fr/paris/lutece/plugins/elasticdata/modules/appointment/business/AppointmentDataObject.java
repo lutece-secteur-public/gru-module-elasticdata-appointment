9a5d5e1e63ca7995d1cb1e5a013fcb46937ab09e
laurent payen <laurent.payen@gmail.com>
2017-07-20 11:46:33 +0200

    formatter

--- src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataObject.java
+++ src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataObject.java
@@ -11,106 +11,117 @@ import fr.paris.lutece.plugins.appointment.business.slot.Slot;
 import fr.paris.lutece.plugins.appointment.service.SlotService;
 import fr.paris.lutece.plugins.elasticdata.business.AbstractDataObject;
 
-public class AppointmentDataObject extends AbstractDataObject {
-	private int _nIdAppointment;
-	private int _nIdSlot;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
-	private Date _dateStartAppointment;
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
-	private Date _dateEndAppointment;
+public class AppointmentDataObject extends AbstractDataObject
+{
+    private int _nIdAppointment;
+    private int _nIdSlot;
+    @JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm" )
+    private Date _dateStartAppointment;
+    @JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm" )
+    private Date _dateEndAppointment;
 
-	/**
-	 * Constructor with Appointment
-	 * 
-	 * @param demand
-	 *            The appointment
-	 */
-	public AppointmentDataObject(Appointment appointment) {
-		super();
-		if (appointment != null) {
-			_nIdAppointment = appointment.getIdAppointment();
-			_nIdSlot = appointment.getIdSlot();
-			Slot slot = SlotService.findSlotById(appointment.getIdSlot());
-			_dateStartAppointment = Date.from(slot.getStartingDateTime().atZone(ZoneId.systemDefault()).toInstant());
-			_dateEndAppointment = Date.from(slot.getEndingDateTime().atZone(ZoneId.systemDefault()).toInstant());
-			setTimestamp(Timestamp.valueOf(slot.getStartingDateTime()).getTime());
-		}
-	}
+    /**
+     * Constructor with Appointment
+     * 
+     * @param demand
+     *            The appointment
+     */
+    public AppointmentDataObject( Appointment appointment )
+    {
+        super( );
+        if ( appointment != null )
+        {
+            _nIdAppointment = appointment.getIdAppointment( );
+            _nIdSlot = appointment.getIdSlot( );
+            Slot slot = SlotService.findSlotById( appointment.getIdSlot( ) );
+            _dateStartAppointment = Date.from( slot.getStartingDateTime( ).atZone( ZoneId.systemDefault( ) ).toInstant( ) );
+            _dateEndAppointment = Date.from( slot.getEndingDateTime( ).atZone( ZoneId.systemDefault( ) ).toInstant( ) );
+            setTimestamp( Timestamp.valueOf( slot.getStartingDateTime( ) ).getTime( ) );
+        }
+    }
 
-	/**
-	 * Returns the IdAppointment
-	 * 
-	 * @return The IdAppointment
-	 */
-	public int getIdAppointment() {
-		return _nIdAppointment;
-	}
+    /**
+     * Returns the IdAppointment
+     * 
+     * @return The IdAppointment
+     */
+    public int getIdAppointment( )
+    {
+        return _nIdAppointment;
+    }
 
-	/**
-	 * Sets the IdAppointment
-	 * 
-	 * @param nIdAppointment
-	 *            The IdAppointment
-	 */
-	public void setIdAppointment(int nIdAppointment) {
-		_nIdAppointment = nIdAppointment;
-	}
+    /**
+     * Sets the IdAppointment
+     * 
+     * @param nIdAppointment
+     *            The IdAppointment
+     */
+    public void setIdAppointment( int nIdAppointment )
+    {
+        _nIdAppointment = nIdAppointment;
+    }
 
-	/**
-	 * Get the id of the slot
-	 * 
-	 * @return The id of the slot
-	 */
-	public int getIdSlot() {
-		return _nIdSlot;
-	}
+    /**
+     * Get the id of the slot
+     * 
+     * @return The id of the slot
+     */
+    public int getIdSlot( )
+    {
+        return _nIdSlot;
+    }
 
-	/**
-	 * Set the id of the slot
-	 * 
-	 * @param nIdSlot
-	 *            The id of the slot
-	 */
-	public void setIdSlot(int nIdSlot) {
-		this._nIdSlot = nIdSlot;
-	}
+    /**
+     * Set the id of the slot
+     * 
+     * @param nIdSlot
+     *            The id of the slot
+     */
+    public void setIdSlot( int nIdSlot )
+    {
+        this._nIdSlot = nIdSlot;
+    }
 
-	/**
-	 * Get the date of start appointment
-	 * 
-	 * @return the date of start appointment
-	 */
-	public Date getStartAppointment() {
-		return _dateStartAppointment;
-	}
+    /**
+     * Get the date of start appointment
+     * 
+     * @return the date of start appointment
+     */
+    public Date getStartAppointment( )
+    {
+        return _dateStartAppointment;
+    }
 
-	/**
-	 * Set the date of start appointment
-	 * 
-	 * @param date
-	 *            The date of start appointment
-	 */
-	public void setStartAppointment(Date date) {
-		this._dateStartAppointment = date;
-	}
+    /**
+     * Set the date of start appointment
+     * 
+     * @param date
+     *            The date of start appointment
+     */
+    public void setStartAppointment( Date date )
+    {
+        this._dateStartAppointment = date;
+    }
 
-	/**
-	 * Get the date of end appointment
-	 * 
-	 * @return the date of end appointment
-	 */
-	public Date getEndAppointment() {
-		return _dateEndAppointment;
-	}
+    /**
+     * Get the date of end appointment
+     * 
+     * @return the date of end appointment
+     */
+    public Date getEndAppointment( )
+    {
+        return _dateEndAppointment;
+    }
 
-	/**
-	 * Set the date of end appointment
-	 * 
-	 * @param date
-	 *            The date of end appointment
-	 */
-	public void setEndAppointment(Date date) {
-		this._dateEndAppointment = date;
-	}
+    /**
+     * Set the date of end appointment
+     * 
+     * @param date
+     *            The date of end appointment
+     */
+    public void setEndAppointment( Date date )
+    {
+        this._dateEndAppointment = date;
+    }
 
 }
--- src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
+++ src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
@@ -3,7 +3,6 @@ package fr.paris.lutece.plugins.elasticdata.modules.appointment.business;
 import java.util.ArrayList;
 import java.util.Collection;
 
-
 import fr.paris.lutece.plugins.appointment.business.AppointmentFilter;
 import fr.paris.lutece.plugins.appointment.business.appointment.Appointment;
 import fr.paris.lutece.plugins.appointment.service.AppointmentService;
@@ -21,7 +20,7 @@ public class AppointmentDataSource extends AbstractDataSource implements DataSou
         AppointmentFilter appointmentFilter = new AppointmentFilter( );
         Collection<DataObject> collResult = new ArrayList<DataObject>( );
 
-        for ( Appointment appointment : AppointmentService.findListAppointmentsByFilter(appointmentFilter ) )
+        for ( Appointment appointment : AppointmentService.findListAppointmentsByFilter( appointmentFilter ) )
         {
 
             collResult.add( new AppointmentDataObject( appointment ) );
