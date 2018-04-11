b910036d2b18586601568c8d5fb260f8f9de1c51
laurent payen <laurent.payen@gmail.com>
2018-04-11 17:12:01 +0200

    RENDEZVOUS-340 : Rename and move appointmentform DTO

--- src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
+++ src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 import fr.paris.lutece.plugins.appointment.business.appointment.Appointment;
 import fr.paris.lutece.plugins.appointment.service.AppointmentService;
-import fr.paris.lutece.plugins.appointment.web.dto.AppointmentFilter;
+import fr.paris.lutece.plugins.appointment.web.dto.AppointmentFilterDTO;
 import fr.paris.lutece.plugins.elasticdata.business.AbstractDataSource;
 import fr.paris.lutece.plugins.elasticdata.business.DataObject;
 import fr.paris.lutece.plugins.elasticdata.business.DataSource;
@@ -17,7 +17,7 @@ public class AppointmentDataSource extends AbstractDataSource implements DataSou
     public Collection<DataObject> getDataObjects( )
     {
 
-        AppointmentFilter appointmentFilter = new AppointmentFilter( );
+        AppointmentFilterDTO appointmentFilter = new AppointmentFilterDTO( );
         Collection<DataObject> collResult = new ArrayList<DataObject>( );
 
         for ( Appointment appointment : AppointmentService.findListAppointmentsByFilter( appointmentFilter ) )
