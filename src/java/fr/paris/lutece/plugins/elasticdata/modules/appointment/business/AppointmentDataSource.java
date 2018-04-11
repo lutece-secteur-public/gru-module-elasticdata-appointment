43df8d8dd9425845bfcf2b42907da30080ef54b5
laurent payen <laurent.payen@gmail.com>
2018-04-11 16:35:35 +0200

    RENDEZVOUS-340 : BO : Lost of some global parameters

--- src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
+++ src/java/fr/paris/lutece/plugins/elasticdata/modules/appointment/business/AppointmentDataSource.java
@@ -3,9 +3,9 @@ package fr.paris.lutece.plugins.elasticdata.modules.appointment.business;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import fr.paris.lutece.plugins.appointment.business.AppointmentFilter;
 import fr.paris.lutece.plugins.appointment.business.appointment.Appointment;
 import fr.paris.lutece.plugins.appointment.service.AppointmentService;
+import fr.paris.lutece.plugins.appointment.web.dto.AppointmentFilter;
 import fr.paris.lutece.plugins.elasticdata.business.AbstractDataSource;
 import fr.paris.lutece.plugins.elasticdata.business.DataObject;
 import fr.paris.lutece.plugins.elasticdata.business.DataSource;
